6c7bb90e75c6e7ccf1a021ddd4833b0eb9a4bb17
Michael Welford <68247065+mwelford3@users.noreply.github.com>
2024-02-21 15:32:45 -0600

    Add files via upload

---
+++ Thesis/read_files.py
@@ -0,0 +1,14 @@
+import os
+
+def read_files():
+    for f in os.listdir('source'):
+        if f.endswith('.maf'):
+        
+            # Extract the file_name.
+            # Open the file
+            # Do etc.
+            print(f)
+            
+            
+            
+read_files()
